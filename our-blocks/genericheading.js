ac860b62cd7aa9193072abc4545a76686e8ff8a1
Piotr Filipiak <kiddhkane@gmail.com>
2022-12-05 22:06:27 +0100

    Displaying the proper data in banner on frontend.

--- our-blocks/genericheading.js
+++ our-blocks/genericheading.js
@@ -58,6 +58,23 @@ function EditComponent(props) {
   );
 }
 
-function SaveComponent() {
-  return <div>Heading block here.</div>;
+function SaveComponent(props) {
+  function createTagName() {
+    switch (props.attributes.size) {
+      case "large":
+        return "h1";
+      case "medium":
+        return "h2";
+      case "small":
+        return "h3";
+    }
+  }
+
+  return (
+    <RichText.Content
+      tagName={createTagName()}
+      value={props.attributes.text}
+      className={`headline headline--${props.attributes.size}`}
+    />
+  );
 }

df9730a40b65ec06b54ab4fe1e5a61af8fd770f9
Piotr Filipiak <kiddhkane@gmail.com>
2022-12-05 21:12:37 +0100

    You can add bold / italic text.

--- our-blocks/genericheading.js
+++ our-blocks/genericheading.js
@@ -1,13 +1,32 @@
 import { registerBlockType } from "@wordpress/blocks";
+import { RichText } from "@wordpress/block-editor";
 
 registerBlockType("ourblocktheme/genericheading", {
   title: "Generic Heading",
+  attributes: {
+    text: { type: "string" },
+    size: { type: "string", default: "large" },
+  },
   edit: EditComponent,
   save: SaveComponent,
 });
 
-function EditComponent() {
-  return <div>Hello</div>;
+function EditComponent(props) {
+  function handleTextChange(x) {
+    props.setAttributes({ text: x });
+  }
+
+  return (
+    <>
+      <RichText
+        tagName="h1"
+        allowedFormats={["core/bold", "core/italic"]}
+        className={`headline headline--${props.attributes.size}`}
+        value={props.attributes.text}
+        onChange={handleTextChange}
+      />
+    </>
+  );
 }
 
 function SaveComponent() {

744558e165e3044f1dc796b567abbd4605841bc3
Piotr Filipiak <kiddhkane@gmail.com>
2022-12-08 23:30:22 +0100

    User can choose an image and it's save in the db.

--- our-blocks/banner.js
+++ our-blocks/banner.js
@@ -1,5 +1,11 @@
-import { InnerBlocks } from "@wordpress/block-editor";
+import {
+  InnerBlocks,
+  InspectorControls,
+  MediaUpload,
+  MediaUploadCheck,
+} from "@wordpress/block-editor";
 import { registerBlockType } from "@wordpress/blocks";
+import { Button, PanelBody, PanelRow } from "@wordpress/components";
 
 registerBlockType("ourblocktheme/banner", {
   title: "Banner",
@@ -8,12 +14,13 @@ registerBlockType("ourblocktheme/banner", {
   },
   attributes: {
     align: { type: "string", default: "full" },
+    imgID: { type: "number" },
   },
   edit: EditComponent,
   save: SaveComponent,
 });
 
-function EditComponent() {
+function EditComponent(props) {
   const useLater = (
     <>
       <h1 className="headline headline--large">Welcome!</h1>
@@ -30,24 +37,46 @@ function EditComponent() {
     </>
   );
 
+  function onFileSelect(x) {
+    props.setAttributes({ imgID: x.id });
+  }
+
   return (
-    <div className="page-banner">
-      <div
-        className="page-banner__bg-image"
-        style={{
-          backgroundImage:
-            "url('/wp-content/themes/fictional-block-theme/images/library-hero.jpg')",
-        }}
-      ></div>
-      <div className="page-banner__content container t-center c-white">
-        <InnerBlocks
-          allowedBlocks={[
-            "ourblocktheme/genericheading",
-            "ourblocktheme/genericbutton",
-          ]}
-        />
+    <>
+      <InspectorControls>
+        <PanelBody title="Background" initialOpen={true}>
+          <PanelRow>
+            <MediaUploadCheck>
+              <MediaUpload
+                onSelect={onFileSelect}
+                value={props.attributes.imgID}
+                render={({ open }) => {
+                  return <Button onClick={open}>Choose image</Button>;
+                }}
+              />
+            </MediaUploadCheck>
+          </PanelRow>
+        </PanelBody>
+      </InspectorControls>
+
+      <div className="page-banner">
+        <div
+          className="page-banner__bg-image"
+          style={{
+            backgroundImage:
+              "url('/wp-content/themes/fictional-block-theme/images/library-hero.jpg')",
+          }}
+        ></div>
+        <div className="page-banner__content container t-center c-white">
+          <InnerBlocks
+            allowedBlocks={[
+              "ourblocktheme/genericheading",
+              "ourblocktheme/genericbutton",
+            ]}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 

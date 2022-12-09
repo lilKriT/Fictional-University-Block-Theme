3ec3d4369aaf1bb0976eefe4e49d3b2c691dd6a9
Piotr Filipiak <kiddhkane@gmail.com>
2022-12-09 18:19:30 +0100

    Styled the placeholder.

--- our-blocks/eventsandblogs.js
+++ our-blocks/eventsandblogs.js
@@ -1,13 +1,17 @@
 wp.blocks.registerBlockType("ourblocktheme/eventsandblogs", {
   title: "Events and Blogs",
   supports: {
-    align: ["full"],
+    // align: ["full"],
   },
   attributes: {
     align: { type: "string", default: "full" },
   },
   edit: function () {
-    return wp.element.createElement("div", null, "This is a placeholder");
+    return wp.element.createElement(
+      "div",
+      { className: "our-placeholder-block" },
+      "Events and blogs placeholder"
+    );
   },
   save: function () {
     return null;

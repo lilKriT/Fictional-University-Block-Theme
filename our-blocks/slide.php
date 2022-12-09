2018265678653ba2676152c0fd6c23dcd6caf1a0
Piotr Filipiak <kiddhkane@gmail.com>
2022-12-09 21:07:10 +0100

    Added slides.

--- our-blocks/slide.php
+++ our-blocks/slide.php
@@ -1,1 +1,15 @@
-<h1>This is a slide</h1>
+<?php
+
+if (!$attributes['imgURL']) {
+    $attributes['imgURL'] = get_theme_file_uri('/images/library-hero.jpg');
+}
+
+?>
+
+<div class="hero-slider__slide" style="background-image: url('<?php echo $attributes['imgURL'] ?>')">
+    <div class="hero-slider__interior container">
+        <div class="hero-slider__overlay t-center">
+            <?php echo $content; ?>
+        </div>
+    </div>
+</div>
